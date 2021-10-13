f14d5a25b2dda281725f4a9ecdf68f74d07d002b
Chamosiala <alexgroza24@gmail.com>
2021-10-13 11:26:07 +0300

    first attempt at splitting a path into two

--- src/components/Localitate.jsx
+++ src/components/Localitate.jsx
@@ -1,12 +1,20 @@
 import { Tooltip } from '@chakra-ui/tooltip';
 
-const Localitate = ({ className, fill, onClick, d, strokeWidth }) => {
+const Localitate = ({
+  className,
+  fill,
+  onClick,
+  transform,
+  d,
+  strokeWidth,
+}) => {
   return fill === 'red' ? (
     <Tooltip label={className} placement="top" defaultIsOpen>
       <path
         className={className}
         fill={fill}
         onClick={onClick}
+        transform={transform}
         d={d}
         stroke-width={strokeWidth}
       />
@@ -16,6 +24,7 @@ const Localitate = ({ className, fill, onClick, d, strokeWidth }) => {
       className={className}
       fill={fill}
       onClick={onClick}
+      transform={transform}
       d={d}
       stroke-width={strokeWidth}
     />
